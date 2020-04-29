39ccd7df1b3aca337c15d3b204236ffc2d868fc0
cknduru <cknduru@github.com>
2020-04-29 22:52:32 +0200

    Switched to ExpressJs to enable CORS
    Signed-off-by: cknduru <cknduru@github.com>

--- server/images/nodejs/app.js
+++ server/images/nodejs/app.js
@@ -1,27 +1,30 @@
-const http = require('http');
-
-const hostname = '';
-const port = 3000;
-
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+ 
 function getFoodItemsFromDb()
 {
-	return ["Meat", "Vegetables", "Beans"];
+       return ["Meat", "Vegetables", "Beans"];
 }
 
-function sendFoodItems(response) {
-	// set HTTP response code
-  	response.writeHead(200, {"Content-Type": "application/json"});
+function sendFoodItems(response) 
+{
+    // set HTTP response code
+    response.writeHead(200, {"Content-Type": "application/json"});
 
-  	var json = JSON.stringify({ 
-   	 foodItems: getFoodItemsFromDb(), 
-  	});
-  	response.end(json);
+    var json = JSON.stringify({
+    foodItems: getFoodItemsFromDb(),
+    });
+    response.end(json);
 }
 
-const server = http.createServer((req, res) => {
+app.get('/', function (req, res, next) {
+  //res.json({msg: 'This is CORS-enabled for all origins!'})
   sendFoodItems(res);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
+ 
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
